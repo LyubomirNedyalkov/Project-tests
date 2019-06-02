f988f3cc873c5fbbc5d93b47e7d4ec38b16de5b2
LyubomirNedyalkov <lubo_ned@abv.bg>
2019-06-02 12:02:44 +0300

    Adding Open Close Menu Scene

---
+++ Example Game Objects/Assets/PanelEscToggle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelEscToggle : MonoBehaviour
+{
+    [SerializeField] GameObject panel;
+  
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            panel.gameObject.SetActive(!panel.gameObject.activeSelf);
+        }
+    }
+}
